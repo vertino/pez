aaf50096091916cbb6ce3ef5c3b3436b7a216a32
leekelleher <leekelleher@80829caf-d146-0410-a3cf-db50ef4ba29f>
2008-03-01 23:35:13 +0000

    Upgraded SimplePie to the latest version (v1.1)
    Added extra classes for the HTML/CSS for the web-source. (i.e. digg, delicious, flickr)
    
    Included the "favicons.gif" image/sprite-map (currently "borrowed" from pownce - will create an original version later)

--- pz-includes/functions.php
+++ pz-includes/functions.php
@@ -2,7 +2,7 @@
 if (!defined('ABSPATH'))
 	die('unable to continue...');
 
-include_once(ABSPATH . '/pz-includes/simplepie.inc');
+include_once(ABSPATH . '/pz-includes/simplepie/simplepie.inc');
 
 function combine_feeds( $feed_list, $max_items = 10, $delimiter = '~', $remove_html = true )
 {
@@ -14,17 +14,17 @@ function combine_feeds( $feed_list, $max_items = 10, $delimiter = '~', $remove_h
 	foreach($feed_list as $url)
 	{
 		$feed = new SimplePie();
-		$feed->feed_url($url);
-		$feed->cache_location(ABSPATH . '/pz-content/cache');
-		$feed->replace_headers(true);
+		$feed->set_feed_url($url);
+		$feed->set_cache_location(ABSPATH . '/pz-content/cache');
+		//$feed->replace_headers(true);
 		if ($remove_html)
 			$feed->strip_htmltags(array('img', 'a', 'object', 'embed', 'param', 'iframe', 'p', 'br', 'div', 'span', 'li', 'ul'));
-		$feed->output_encoding(CHARSET);
+		$feed->set_output_encoding(CHARSET);
 		$feed->init();
 		
 		foreach ($feed->get_items(0, $max_items) as $item)
 		{
-			$combined_feed[$item->get_date('U') . $delimiter . $feed->get_feed_title() . $delimiter . $feed->get_feed_link()] = $item;
+			$combined_feed[$item->get_date('U') . $delimiter . $feed->get_title() . $delimiter . $feed->get_link()] = $item;
 		}
 		unset($feed);
 	}
@@ -34,16 +34,18 @@ function combine_feeds( $feed_list, $max_items = 10, $delimiter = '~', $remove_h
 	return array_slice($combined_feed, 0, $max_items);
 }
 
-function link_list( $items )
+function link_list( $items, $delimiter = '~' )
 {
 	if (empty($items))
 		return false;
 	
 	$i = 0;
 	$html = "<ul>\n";
-	foreach($items as $item)
+	$items_count = (sizeof($items) - 1);
+	foreach($items as $key => $item)
 	{
-		$class = ( ($i == 0) ? 'first ' : ( ($i == (sizeof($items) - 1)) ? 'last ' : '' ) ) . 'item';
+		$source = explode($delimiter, $key);
+		$class = ( ($i == 0) ? 'first ' : ( ($i == $items_count) ? 'last ' : '' ) ) . 'item ' . get_domain($source[2]);
 		$html .= '<li><a class="' . $class . '" href="' . $item->get_permalink() . '">' . $item->get_title() . "</a></li>\n";
 		$i++;
 	}
@@ -75,8 +77,9 @@ function flickr_photos( $feed_url, $max_items = 10 )
 	//$items = combine_feeds( array($feed_url), $max_items, '~', false);
 	
 	$feed = new SimplePie();
-	$feed->feed_url($feed_url); 
-	$feed->output_encoding('ISO-8859-1');
+	$feed->set_feed_url($feed_url); 
+	$feed->set_cache_location(ABSPATH . '/pz-content/cache');
+	$feed->set_output_encoding('ISO-8859-1');
 	$feed->init();
 
 	$html = '';
@@ -86,7 +89,7 @@ function flickr_photos( $feed_url, $max_items = 10 )
 		foreach ($feed->get_items(0, $max_items) as $item)
 		{
 			$image = $item->get_description();
-			$image = substr($image, strpos($image, '<img') + 13);
+			$image = substr($image, strpos($image, '<img') + 10);
 			$image = trim(substr($image, 0, strpos($image, "\" width")));
 			$healthy = array("%3A", "%2F");
 			$yummy = array(":", "/");
@@ -94,7 +97,7 @@ function flickr_photos( $feed_url, $max_items = 10 )
 			$imagetn = str_replace('m.jpg', 's.jpg', $image);
 			
 			$html .= '<a href="' . $item->get_permalink() . '">';
-			$html .= '<img src="htt' . $imagetn . '" alt="[flickr photo: ' . $item->get_title() . ']" title="' . $item->get_title() . '" />';
+			$html .= '<img src="' . $imagetn . '" alt="[flickr photo: ' . $item->get_title() . ']" title="' . $item->get_title() . '" />';
 			$html .= "</a>\n";
 		}
 	}
@@ -117,6 +120,7 @@ function profile_list( $deletable = false )
 	
 	$i = 0;
 	$html = "<ul>\n";
+	$profile_count = (sizeof($profile->profiles) - 1);
 	foreach($profile->profiles as $idx => $profile)
 	{
 		if ($deletable)
@@ -128,7 +132,7 @@ function profile_list( $deletable = false )
 			$delete_me .= '</form>';
 		}
 		
-		$class = ( ($i == 0) ? 'first ' : ( ($i == (sizeof($profile->profiles) - 1)) ? 'last ' : '' ) ) . 'item ' . $profile[0];
+		$class = ( ($i == 0) ? 'first ' : ( ($i == $profile_count) ? 'last ' : '' ) ) . 'item ' . $profile[0];
 		$html .= '<li><a class="' . $class . '" rel="me" href="' . sprintf($profile[1][1], $profile[2]) . '">' . $profile[1][0] . '</a>' . $delete_me . "</li>\n";
 		$i++;
 	}
@@ -137,6 +141,14 @@ function profile_list( $deletable = false )
 	unset($profile, $profile, $i);
 	
 	return $html;
+}
+
+function get_domain( $url )
+{
+	$removeables = array('www.', '.com', '.net', '.org', '.gov', '.co', '.uk', '.');
+	$host = parse_url($url, PHP_URL_HOST);
+	$host = str_replace($removeables, '', $host);
+	return $host;
 }
 
 ?>
